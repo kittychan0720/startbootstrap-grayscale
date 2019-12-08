4db6d47e5891e4627e00f69fdb6e33ce0bbf2733
Kitty Chan <kittychan0720@Kittyde-MacBook-Pro.local>
2019-12-09 02:51:36 +0800

    0

--- js/grayscale.js
+++ js/grayscale.js
@@ -40,3 +40,22 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
